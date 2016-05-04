87695d0b04d3c1bf41c8ac6e0573844b763bf581
TPhilippon <terence.philippon@sfr.fr>
2016-05-04 13:42:01 +0200

    edit contour & contourf ok

--- 2_contourf.py
+++ 2_contourf.py
@@ -14,6 +14,8 @@ from pylab import mpl as mpl
 import matplotlib.pyplot as plt
 from matplotlib.colors import Colormap
 from os.path import basename
+from mamba import *
+from scipy import ndimage
 
 #==============================================================================
 # #                             Definitions 
@@ -65,9 +67,6 @@ Colormap.set_under(new_map_chl,color=new_map_chl._lut[0,:]) # Set color for valu
 grays = [(0.33,0.33,0.33)] + [(plt.cm.gray(i)) for i in xrange(1,256)]
 new_map_gray_chl = mpl.colors.LinearSegmentedColormap.from_list('new_map_gray_chl', grays, N=256)
 
-data = glob.glob(path+'*.npy')
-data.sort()
-print data
 
 #==============================================================================
 # #                             Starting loop
@@ -80,8 +79,8 @@ print data
 #x = np.zeros(10, dtype= [('date', 'S15', 1), ('seuil', 'int8', 6), ('zrSEUIL', 'int8', 1)])
 
 # Rappel : seuilx100 
-#seuils = np.array([(0.10), (0.15), (0.20), (0.25), (0.30), (0.40)])
-seuils = np.array([(0.20), (0.40)])
+seuils = np.array([(0.05), (0.10), (0.15), (0.20), (0.30), (0.40), (10)])
+#seuils = np.array([(0.20), (0.40)])
 #Seuils = seuils*100
 
 
@@ -90,13 +89,29 @@ i = 0
 for myfile in data:
     print 'reading data...'
     print myfile
-    r = np.load(myfile)
+    r = np.load(myfile)                        # numpy
 #    x['date'][i] = basename(myfile[:-4])
 #    x['seuil'][i] = Seuils
 #    x['zrSEUIL'][i] = r
 #    i += 1
     
-    print 'end'
+    zrcontour = plt.contourf(r,seuils, origin='upper')
+#    plt.contour(r,seuils, origin='upper')
+    A = ndimage.binary_opening(r)
+    plt.plot()
+    # --- MbImage ---
+#    im = imageMb(myfile)
+#    negate(im, im)
+#    closeHoles(im,im)
+#    plt.imshow(im)
+#    im.save(outpath+basename(myfile[:-4])+'.png')    
+    # --- MbImage ---
+    
+#    fig1 = plt.gcf()
+    plt.imshow(zrcontour, norm=norm_chl, origin='upper', cmap=new_map_chl,)
+#    plt.show()
+    
+print 'end'
 
 
 
